b735d1a2814009e370158fe350dffdac3a3c08f1
Yanze LIU <yanzeliu.france@gmail.com>
2015-04-23 13:28:06 -0700

    update for capitalize_first_letter

--- js/capitalize_first_letter.js
+++ js/capitalize_first_letter.js
@@ -7,3 +7,16 @@ function LetterCapitalize(str) { //?????
   return capitalized.join(" ");
 }
 console.log(LetterCapitalize("hello how are you?"));
+
+
+//2nd
+
+function LetterCapitalize(str) {
+  var string = str.split(" ");
+  var letters = [];
+  for (var i = 0; i < string.length; i++) {
+    var firstLetter = string[i].slice(0, 1).toUpperCase();
+    letters.push(firstLetter + string[i].slice(1));
+  }
+  return letters.join(" ");
+}
