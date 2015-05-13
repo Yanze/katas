da3e618fce1e93016de13e9cfd4e45be2f9c3a94
Yanze LIU <yanzeliu.france@gmail.com>
2015-05-13 11:02:25 -0700

    Update findGreatest_commonFactor.js

--- js/findGreatest_commonFactor.js
+++ js/findGreatest_commonFactor.js
@@ -1,3 +1,14 @@
-function mygcd(x, y) {
-  return y == 0 ? x : mygcd(y, x % y);
+function greatestCommonFactor(x, y) {
+  return y == 0 ? x : greatestCommonFactor(y, x % y);
 }
+
+function greatestCommonFactor(x, y) {
+    var min = Math.min(x, y);
+    for (var i = min; i > 0; i--) {
+        if (x % i == 0 && y % i == 0) {
+            return i;
+        }
+    }
+}
+
+
